1f36d50427a4ecc922a710b0742ff8bd30762572
Jason <jason13.deng@gmail.com>
2026-02-19 17:29:47 +0900

    On "More on State"

---
+++ src/ConversionPractice.jsx
@@ -0,0 +1,16 @@
+function Practice(){
+    return (
+        <>
+        <h1>Test sstitle</h1>
+        <svg>
+            <circle cx="25" cy="75" r="20" stroke="green" strokeWidth="2" />
+        </svg>
+        <form>
+            <input type="text" />
+        </form>
+        
+        </>
+    )
+}
+
+export default Practice
---
+++ src/Food.jsx
@@ -0,0 +1,10 @@
+function FavoriteFood() {
+	return (
+		<>
+			<h1>My favorite food to eat currently include: </h1>
+			<p>bibimbap, shredded chicken breast, karaage</p>
+		</>
+	);
+}
+
+export { FavoriteFood };
